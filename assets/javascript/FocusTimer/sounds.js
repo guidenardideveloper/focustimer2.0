37fae3907da9f58166a1504725becd71a7727299
Guilherme Denardi <gdenardi@outlook.com>
2023-08-18 14:48:31 -0300

    adding loop event to audios

--- assets/javascript/FocusTimer/sounds.js
+++ assets/javascript/FocusTimer/sounds.js
@@ -2,3 +2,8 @@ export const forestAudio = new Audio('./assets/sounds/forest.wav')
 export const rainAudio = new Audio('./assets/sounds/rain.wav')
 export const coffeshopAudio = new Audio('./assets/sounds/coffeshop.wav')
 export const fireplaceAudio = new Audio('./assets/sounds/fireplace.wav')
+
+forestAudio.loop = true
+rainAudio.loop = true
+coffeshopAudio.loop = true
+fireplaceAudio.loop = true
