18fc5bb800f60a2620e6c34e5f1aa5fe6ed1c898
Guilherme Denardi <gdenardi@outlook.com>
2023-08-18 12:04:15 -0300

    changing the audio path

--- assets/javascript/FocusTimer/sounds.js
+++ assets/javascript/FocusTimer/sounds.js
@@ -1,4 +1,4 @@
-export const forestAudio = new Audio('../assets/sounds/forest.wav')
-export const rainAudio = new Audio('../assets/sounds/rain.wav')
-export const coffeshopAudio = new Audio('../assets/sounds/coffeshop.wav')
-export const fireplaceAudio = new Audio('../assets/sounds/fireplace.wav')
+export const forestAudio = new Audio('./assets/sounds/forest.wav')
+export const rainAudio = new Audio('./assets/sounds/rain.wav')
+export const coffeshopAudio = new Audio('./assets/sounds/coffeshop.wav')
+export const fireplaceAudio = new Audio('./assets/sounds/fireplace.wav')
